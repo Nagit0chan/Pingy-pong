e83547995798b2a21d623cf80ded8883d4f07a6a
Sophia <130580654+Nagit0chan@users.noreply.github.com>
2023-04-26 18:26:33 +0300

    v 2.0

--- Pingy_pong.py
+++ Pingy_pong.py
@@ -15,6 +15,8 @@ mixer.music.play(-1)
 font.init()
 font1 = font.SysFont('Arial', 24)
 font2 = font.SysFont('Arial', 40)
+lose1 = font2.render('PLAYER 1 LOSE!', True, (180, 0 , 0))
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180, 0 , 0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, player_size):
@@ -38,11 +40,22 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 35 0:
+        if keys[K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
 
-player1 = Player('sirok.jpg', 60, 175, 7, (50, 150))
-player2 = Player('sirok.jpg', 590, 175, 7, (50, 150))
+class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_size):
+        super().__init__(player_image, player_x, player_y, player_speed, player_size)
+        self.speed_x = player_speed
+        self.speed_y = player_speed
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+player1 = Player('sirok.jpg', 70, 175, 7, (50, 150))
+player2 = Player('sirok.jpg', 580, 175, 7, (50, 150))
+ball = Ball('gym_teacher.jpg', 150, 225, 3, (50, 50))
+
 
 clock = time.Clock()
 game = True
@@ -62,5 +75,22 @@ while game:
         player2.reset()
         player2.update_r()
 
-    clock.tick(FPS)
-    display.update()
+        ball.reset()
+        ball.update()
+
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            ball.speed_x *= -1
+
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            ball.speed_y *= -1
+
+        if ball.rect.x < 50:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > 650:
+            finish = True
+            window.blit(lose2, (200, 200))
+
+        clock.tick(FPS)
+        display.update()
